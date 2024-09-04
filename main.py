1d3ee3208bf1e4c925345b5ede7fc80997c0f2ce
Aksel <aknea001@osloskolen.no>
2024-09-04 13:24:25 +0200

    check if input is just a letter

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ word = "testing"
 def main(word):
     letterAmounts = ""
     letterNum = 0
+    letterList = []
 
     for i in word:
         letterAmounts += "_ "
         letterNum += 1
+        letterList.append(i)
 
     print(f"{letterAmounts} \n\nThere are {letterNum} letters in the word..")
+    
+    while True:
+        while True:
+            try:
+                guess = input("Guess a letter..\n>> ")
+                if guess.isalpha() and len(guess) == 1:
+                    break
+            except Exception as e:
+                print(f"ERROR getting letter: {e}")
+        print("hi")
+        break
 
 if __name__ == "__main__":
     main(word)
