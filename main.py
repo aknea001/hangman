07eab68fd5762e5188525c7b133b75298fa32d79
Aksel <aknea001@osloskolen.no>
2024-09-04 13:05:12 +0200

    fixed last commit

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ word = "testing"
 
 def main(word):
     letterAmounts = ""
+    letterNum = 0
 
     for i in word:
         letterAmounts += "_ "
+        letterNum += 1
 
-    print(f"{letterAmounts} \n")
+    print(f"{letterAmounts} \n\nThere are {letterNum} letters in the word..")
 
 if __name__ == "__main__":
     main(word)
