bace1babcb35b20369f0306404e82edc2251ba1a
Aksel <aknea001@osloskolen.no>
2024-09-05 12:40:37 +0200

    added functions

--- main.py
+++ main.py
@@ -10,17 +10,65 @@ if response.status_code == 200:
 else:
     print(f"Failed to retrieve word: {response.status_code}")
 
+letterAmounts = ""
+letterNum = 0
+mistakes = 0
+lives = 6
+letterList = []
+revealAnswer = []
+guessing = False
+
+def guessHandling(guess):
+    if guess in letterList:
+        for i in range(len(letterList)):
+            if letterList[i] == guess:
+                revealAnswer[i] = guess
+    else:
+        mistakes += 1
+            
+    if endHandling():
+        return
+    
+    # print(letterList)
+
+    print(open(f"hangmanPics/hangman{mistakes}.txt", "r").read())
+    print(" ".join(revealAnswer))
+
+def endHandling():
+    if "_" not in revealAnswer:
+        print(open(f"hangmanPics/hangman0.txt", "r").read())
+        print("Good Job! You did it!")
+
+        return "win"
+    elif mistakes >= lives:
+        print(open("hangmanPics/dead.txt", "r").read())
+        print("You died :(\n x_x\n")
+        print(f"The word was: {' '.join(letterList)}")
+
+        return "dead"
+    else:
+        return
+
+def getGuess():
+    while True:
+        try:
+            if guessing:
+                guess = input("Which word do u want to guess?\n>> ")
+                if guess == "".join(letterList):
+                    print("yippie")
+            guess = input("Guess a letter..\n>> ")
+            if guess[0] == "-":
+                if guess == "-g":
+                    guessing = True
+                    break
+            elif guess.isalpha() and len(guess) == 1:
+                break
+            else:
+                print("Please guess only an alphabetical letter\n\n")
+        except Exception as e:
+            print(f"ERROR getting letter: {e}")
 
 def main(word):
-    letterAmounts = ""
-    letterNum = 0
-    mistakes = 0
-    lives = 6
-    letterList = []
-    revealAnswer = []
-
-    guessing = False
-
     for i in word:
         letterAmounts += "_ "
         letterNum += 1
@@ -29,46 +77,8 @@ def main(word):
 
     print(open("hangmanPics/hangman0.txt", "r").read())
     print(f"{letterAmounts} \n\nThere are {letterNum} letters in the word..")
-    
-    while True:
-        if "_" not in revealAnswer:
-            break
-
-        while True:
-            try:
-                if guessing:
-                    guess = input("Which word do u want to guess?\n>> ")
-                    if guess == "".join(letterList):
-                        print("yippie")
-                guess = input("Guess a letter..\n>> ")
-                if guess[0] == "-":
-                    if guess == "-g":
-                        guessing = True
-                        break
-                elif guess.isalpha() and len(guess) == 1:
-                    break
-                else:
-                    print("Please guess only an alphabetical letter\n\n")
-            except Exception as e:
-                print(f"ERROR getting letter: {e}")
-        
-        if guess in letterList:
-            for i in range(len(letterList)):
-                if letterList[i] == guess:
-                    revealAnswer[i] = guess
-        else:
-            mistakes += 1
-            
-        # print(letterList)
-        print(open(f"hangmanPics/hangman{mistakes}.txt", "r").read())
-        print(" ".join(revealAnswer))
-
-        if mistakes >= lives:
-            print(open("hangmanPics/dead.txt", "r").read())
-            print("You died :(\n x_x\n")
-            print(f"The word was: {' '.join(letterList)}")
-
-            return
 
 if __name__ == "__main__":
-    main(word)
+    #main(word)
+    if endHandling():
+        print("whatever")
